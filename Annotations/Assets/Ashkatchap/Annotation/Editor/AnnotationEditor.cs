5f5af9d637a77e07b9ea68d50bb31092d2468e9b
Andrés <forestrf@gmail.com>
2019-07-13 01:28:12 +0200

    Added text wrapping and rich text support for the annotations.

--- Annotations/Assets/Ashkatchap/Annotation/Editor/AnnotationEditor.cs
+++ Annotations/Assets/Ashkatchap/Annotation/Editor/AnnotationEditor.cs
@@ -1,20 +1,51 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 
 namespace Ashkatchap.Annotation {
 	[CustomEditor(typeof(Annotation))]
-	public class AnnotationEditor : Editor {
+	public class AnnotationEditorMonoBehaviour : AnnotationEditorBase { }
+
+	[CustomEditor(typeof(AnnotationSMB))]
+	public class AnnotationEditorSMB : AnnotationEditorBase { }
+
+	public abstract class AnnotationEditorBase : Editor {
+		static GUIStyle textAreaStyle;
 		SerializedProperty text;
 
 		void OnEnable() {
 			text = serializedObject.FindProperty("text");
 		}
-		
+
 		public override void OnInspectorGUI() {
 			EditorGUILayout.HelpBox("Annotations are stripped from builds", MessageType.Info);
 
+			if (textAreaStyle == null) {
+				textAreaStyle = GUI.skin.textArea;
+				textAreaStyle.wordWrap = true;
+				textAreaStyle.richText = true;
+			}
+
 			serializedObject.Update();
-			text.stringValue = EditorGUILayout.TextArea(text.stringValue);
+			text.stringValue = WithoutSelectAll(() => EditorGUILayout.TextArea(text.stringValue, textAreaStyle));
 			serializedObject.ApplyModifiedProperties();
+		}
+
+		// https://stackoverflow.com/questions/44097608/how-can-i-stop-immediate-gui-from-selecting-all-text-on-click/44097609#44097609
+		private T WithoutSelectAll<T>(Func<T> guiCall) {
+			bool preventSelection = (Event.current.type == EventType.MouseDown);
+
+			Color oldCursorColor = GUI.skin.settings.cursorColor;
+
+			if (preventSelection)
+				GUI.skin.settings.cursorColor = new Color(0, 0, 0, 0);
+
+			T value = guiCall();
+
+			if (preventSelection)
+				GUI.skin.settings.cursorColor = oldCursorColor;
+
+			return value;
 		}
 	}
 }
