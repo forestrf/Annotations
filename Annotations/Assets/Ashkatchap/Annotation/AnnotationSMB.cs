646594b4771f9f6ee17d5894582c4efb2f767862
Andrés <forestrf@gmail.com>
2019-07-13 01:27:40 +0200

    Added annotations for State Machine Behaviours

---
+++ Annotations/Assets/Ashkatchap/Annotation/AnnotationSMB.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Ashkatchap.Annotation {
+	[ExecuteInEditMode]
+	public class AnnotationSMB : StateMachineBehaviour {
+		public string text;
+
+		private void Awake() {
+			this.hideFlags = HideFlags.DontSaveInBuild;
+		}
+	}
+}
